c488c7f3d880ba0dcb257e2d3039f112528a29a5
Raves <117028983+Raves16@users.noreply.github.com>
2023-06-29 16:44:02 +0200

    Add files via upload

---
+++ Draw.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+
+int draw_quadratic_function(std::string function);
+
+int draw_hyperbola_function(std::string function);
+
+int draw_modulus_function(std::string function);
